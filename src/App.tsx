8c1c4275d0c13122d221625f143e7651be0c2665
Ahmed Boujelben <ahmed.hamdi.boujelben@gmail.com>
2024-03-24 19:01:07 +0000

    Added a subtitle (#15)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
 
 function App() {
-  return <h1>Graphite Playground</h1>;
+  return (
+    <>
+      <h1>Graphite Playground</h1>
+      <h2>Made for testing purposes!</h2>
+    </>
+  );
 }
 
 export default App;
